182c738f936efb64aa7454e930b12fb171d33bcc
sobulik <69842563+sobulik@users.noreply.github.com>
2020-08-21 17:23:07 +0200

    Dump yaml in utf-8

--- persistence/yaml.py
+++ persistence/yaml.py
@@ -4,10 +4,10 @@ import yaml
 
 def load(ifile):
     """load yaml from an input file"""
-    with open(ifile, "r", encoding='utf-8') as f:
+    with open(ifile, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
 
 def dump(obj, ofile):
     """dump yaml to an output file"""
-    with open(ofile, "w") as f:
+    with open(ofile, "w", encoding="utf-8") as f:
         return yaml.safe_dump(obj, f, allow_unicode=True, indent=4)
