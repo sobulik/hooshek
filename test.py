e728f20de9a02c374302bb5a445fb106a08bf6c0
Jan Halama <jenik.halama@gmail.com>
2020-09-06 18:49:30 +0200

    test: clear data dir if already exists

--- test.py
+++ test.py
@@ -17,6 +17,8 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.dir_data_source = os.path.join(os.path.dirname(__file__), "test", "resources", "2020-skuhrovska-lyze")
         self.dir_data_target = os.path.join(os.path.dirname(__file__), "test", "data", str(random.randrange(1024)).rjust(4, "0"))
+        if os.path.isdir(self.dir_data_target) == True:
+            shutil.rmtree(self.dir_data_target)
         os.makedirs(self.dir_data_target)
         for f in ("athletes.yaml", "clubs.yaml", "event.yaml", "finish.yaml"):
             shutil.copy(os.path.join(self.dir_data_source, f), os.path.join(self.dir_data_target, f))
