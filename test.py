c7a4d833078c35fb89fbd41ff87f36c04cfe3ecd
Jan Halama <jenik.halama@gmail.com>
2020-09-06 18:47:34 +0200

    test: fix unittests tests on Windows box

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 import unittest
 import os
+import sys  
 
 class Test(unittest.TestCase):
 
@@ -29,34 +30,34 @@ class Test(unittest.TestCase):
 
     def testAthletesSorted(self):
         """Athletes are sorted correctly"""
-        subprocess.run([os.path.join(os.path.dirname(__file__), "athletes.py")], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "athletes.py")], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "athletes-sorted.yaml"), os.path.join(self.dir_data_target, "athletes-sorted.yaml")))
 
     def testStartList(self):
         """Start list is correct"""
-        subprocess.run([os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml")))
 
     def testStartText(self):
         """Start list text file is correct"""
-        subprocess.run([os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start.txt"), os.path.join(self.dir_data_target, "start.txt")))
 
     def testStartClub(self):
         """Start list grouped by club file is correct"""
-        subprocess.run([os.path.join(os.path.dirname(__file__), "start.py"), "--clubs"], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py"), "--clubs"], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start-clubs.txt"), os.path.join(self.dir_data_target, "start-clubs.txt")))
 
     def testResults(self):
         """Result list is correct"""
         shutil.copy(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml"))
-        subprocess.run([os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "results.yaml"), os.path.join(self.dir_data_target, "results.yaml")))
 
     def testResultsText(self):
         """Result list text file is correct"""
         shutil.copy(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml"))
-        subprocess.run([os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
         self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "results.txt"), os.path.join(self.dir_data_target, "results.txt")))
 
 
