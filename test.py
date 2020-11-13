f05e11af506d26c34a5c2ae379cfb3e8bed7d1a8
sobulik <69842563+sobulik@users.noreply.github.com>
2020-11-13 10:12:26 +0100

    Enhance testing to cover a list of events

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import filecmp
-import random
 import shutil
 import subprocess
 import unittest
+import inspect
 import os
 import sys
 
@@ -15,14 +15,12 @@ class Test(unittest.TestCase):
         pass
 
     def setUp(self):
-        self.dir_data_source = os.path.join(os.path.dirname(__file__), "test", "resources", "2020-skuhrovska-lyze")
-        self.dir_data_target = os.path.join(os.path.dirname(__file__), "test", "data", str(random.randrange(4048)).rjust(4, "0"))
+        self.dir_target = os.path.join(os.path.dirname(__file__), "test", "data", self.id())
         try:
-            os.makedirs(self.dir_data_target)
-        except FileExistsError:
-            self.fail()
-        for f in ("athletes.yaml", "clubs.yaml", "event.yaml", "finish.yaml"):
-            shutil.copy(os.path.join(self.dir_data_source, f), os.path.join(self.dir_data_target, f))
+            shutil.rmtree(self.dir_target)
+        except FileNotFoundError:
+            pass
+        os.makedirs(self.dir_target)
 
     def tearDown(self):
         pass
@@ -31,38 +29,63 @@ class Test(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    def testAthletesSorted(self):
+    def _test_athletes_sorted(self, dir_source):
         """Athletes are sorted correctly"""
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "athletes.py")], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "athletes-sorted.yaml"), os.path.join(self.dir_data_target, "athletes-sorted.yaml")))
+        for f in ("event.yaml", "athletes.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "athletes.py")], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "athletes-sorted.yaml"), os.path.join(self.dir_target, "athletes-sorted.yaml")))
 
-    def testStartList(self):
+    def _test_start_list(self, dir_source):
         """Start list is correct"""
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml")))
+        for f in ("event.yaml", "clubs.yaml", "athletes.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "start.py")], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "start.yaml"), os.path.join(self.dir_target, "start.yaml")))
 
-    def testStartText(self):
+    def _test_start_text(self, dir_source):
         """Start list text file is correct"""
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py")], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start.txt"), os.path.join(self.dir_data_target, "start.txt")))
+        for f in ("event.yaml", "clubs.yaml", "athletes.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "start.py")], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "start.txt"), os.path.join(self.dir_target, "start.txt")))
 
-    def testStartClub(self):
+    def _test_start_club(self, dir_source):
         """Start list grouped by club file is correct"""
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "start.py"), "--clubs"], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "start-clubs.txt"), os.path.join(self.dir_data_target, "start-clubs.txt")))
+        for f in ("event.yaml", "clubs.yaml", "athletes.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "start.py"), "--clubs"], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "start-clubs.txt"), os.path.join(self.dir_target, "start-clubs.txt")))
 
-    def testResults(self):
+    def _test_results(self, dir_source):
         """Result list is correct"""
-        shutil.copy(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml"))
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "results.yaml"), os.path.join(self.dir_data_target, "results.yaml")))
+        for f in ("event.yaml", "clubs.yaml", "athletes.yaml", "start.yaml", "finish.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "finish.py")], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "results.yaml"), os.path.join(self.dir_target, "results.yaml")))
 
-    def testResultsText(self):
+    def _test_results_text(self, dir_source):
         """Result list text file is correct"""
-        shutil.copy(os.path.join(self.dir_data_source, "start.yaml"), os.path.join(self.dir_data_target, "start.yaml"))
-        subprocess.run([sys.executable, os.path.join(os.path.dirname(__file__), "finish.py")], cwd=self.dir_data_target)
-        self.assertTrue(filecmp.cmp(os.path.join(self.dir_data_source, "results.txt"), os.path.join(self.dir_data_target, "results.txt")))
+        for f in ("event.yaml", "clubs.yaml", "athletes.yaml", "start.yaml", "finish.yaml"):
+            shutil.copy(os.path.join(dir_source, f), os.path.join(self.dir_target, f))
+
+        subprocess.run([sys.executable, os.path.join(os.path.dirname(os.path.abspath(__file__)), "finish.py")], cwd=self.dir_target)
+        self.assertTrue(filecmp.cmp(os.path.join(dir_source, "results.txt"), os.path.join(self.dir_target, "results.txt")))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    def generator(test, folder):
+        def t(self):
+            test(self, folder)
+        return t
+
+    dir_resources = os.path.join(os.path.dirname(__file__), "test", "resources")
+    for test in inspect.getmembers(Test, predicate=inspect.isfunction):
+        if "_test_" == test[0][:6]:
+            for folder in os.listdir(dir_resources):
+                testName = "test_%s-%s" % (test[0][6:], folder)
+                testFunction = generator(test[1], os.path.join(dir_resources, folder))
+                setattr(Test, testName, testFunction)
+
     unittest.main()
