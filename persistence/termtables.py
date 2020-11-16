d1ec29886e0c55888b965f63d161941a9c0a9ded
sobulik <69842563+sobulik@users.noreply.github.com>
2020-11-16 15:20:37 +0100

    Shorten name field to 21 chars

--- persistence/termtables.py
+++ persistence/termtables.py
@@ -5,9 +5,9 @@ import termtables
 def dump_start(o, ofile, encoding_print):
     """dump start object to an output file"""
     if o["mass"]:
-        header = ["  #", "         jméno         ", "ročník", "klub", "       čas       "]
+        header = ["  #", "        jméno        ", "ročník", "klub", "       čas       "]
     else:
-        header = ["  #", "         jméno         ", "ročník", "klub", "start  ", "       cíl       "]
+        header = ["  #", "        jméno        ", "ročník", "klub", "start  ", "       cíl       "]
     with open(ofile, "w", encoding=encoding_print) as f:
         for race in o["races"]:
             f.write("Kategorie: {0}   Trať: {1}\n".format(race["name"], race["distance"]))
@@ -15,9 +15,9 @@ def dump_start(o, ofile, encoding_print):
                 athletes = list()
                 for a in race["athletes"]:
                     if o["mass"]:
-                        l = (a["id"], (a["name"] + " " + a["surname"])[:23], a["born"], a["club"], "")
+                        l = (a["id"], (a["name"] + " " + a["surname"])[:21], a["born"], a["club"], "")
                     else:
-                        l = (a["id"], (a["name"] + " " + a["surname"])[:23], a["born"], a["club"], a["start"], "")
+                        l = (a["id"], (a["name"] + " " + a["surname"])[:21], a["born"], a["club"], a["start"], "")
                     athletes.append(l)
                 if o["mass"]:
                     alignment = "rlccr"
@@ -29,14 +29,14 @@ def dump_start(o, ofile, encoding_print):
 
 def dump_finish(o, ofile, encoding_print):
     """dump finish object to an output file"""
-    header = [" #", " S", "         jméno         ", "ročník", "klub", "čas  ", "ztráta"]
+    header = [" #", " S", "        jméno        ", "ročník", "klub", "čas  ", "ztráta"]
     with open(ofile, "w", encoding=encoding_print) as f:
         for race in o["races"]:
             f.write("Kategorie: {0} {1}  Trať: {2}\n".format(race["name"], race["desc"], race["distance"]))
             if len(race["athletes"]) > 0:
                 athletes = list()
                 for a in race["athletes"]:
-                    l = (a["rank"], a["rank_sokol"], (a["name"] + " " + a["surname"])[:23], a["born"], a["club"], a["time"], a["diff"])
+                    l = (a["rank"], a["rank_sokol"], (a["name"] + " " + a["surname"])[:21], a["born"], a["club"], a["time"], a["diff"])
                     athletes.append(l)
                 f.write(termtables.to_string(athletes, header=header, alignment="rrlccrr"))
             f.write("\n\n")
