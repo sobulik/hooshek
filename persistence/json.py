3614eb588298bdea25435e22d1ec845a73227c31
Jan Halama <mail@janhalama.cz>
2024-12-23 22:00:44 +0100

    feat: enhance JSON serialization to support datetime and date objects

--- persistence/json.py
+++ persistence/json.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import json
+from datetime import date, datetime
 
 def dump(obj, ofile, encoding_print):
     """dump results to a json file"""
     with open(ofile, 'w', encoding=encoding_print) as f:
-        json.dump(obj, f, indent=2)
+        json.dump(obj, f, indent=2, default=json_serial, ensure_ascii=False)
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
