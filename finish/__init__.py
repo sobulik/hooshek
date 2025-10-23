dc1034348420d8ffa9129e0b9eea2076f84e6c1d
sobulik <69842563+sobulik@users.noreply.github.com>
2025-10-23 18:07:14 +0200

    Start time offset

--- finish/__init__.py
+++ finish/__init__.py
@@ -45,6 +45,9 @@ def fill_category(e, event, aths, race, start, flist):
         if (len(result) == 1 and athlete.start is not None and result[0]["time"] != "00:00:00.0"):
             athlete.finish = util.parseTime(result[0]["time"])
             athlete.time = datetime.datetime.combine(event.date, athlete.finish) - datetime.datetime.combine(event.date, util.parseTime(athlete.start))
+            if not event.mass:
+                athlete.time += datetime.timedelta(hours=event.start.hour, minutes=event.start.minute, seconds=event.start.second)
+
 
     e["finished"] = sorted(filter(lambda a: hasattr(a, "time"), e["started"]), key=lambda a: a.time)
     e["unfinished"] = sorted(filter(lambda a: not hasattr(a, "time"), e["started"]), key=lambda a: a.id)
