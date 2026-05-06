5f594221e8046b5043fd6ec6951ac429bd913c89
sobulik <69842563+sobulik@users.noreply.github.com>
2026-05-06 11:31:22 +0200

    sort athletes test

--- tests/test_athletes.py
+++ tests/test_athletes.py
@@ -1,4 +1,9 @@
+import filecmp
+import pathlib
 import pytest
+import shutil
+import subprocess
+import sys
 
 class TestAthletes:
 
@@ -11,6 +16,16 @@ class TestAthletes:
         "2025-setkani-mistru" \
     )
 
+    root = pathlib.Path(__file__).parents[1]
+    resources = root / "test" / "resources"
+
     @pytest.mark.parametrize("event", events)
-    def test_sort(self, event):
-        assert "skuhrov" in event or "setkani-mistru" in event
+    def test_sort(self, event, tmp_path):
+        source = self.resources / event
+        if (source / "athletes-sorted.yaml").is_file():
+            for f in ("event.yaml", "clubs.yaml", "athletes.yaml"):
+                shutil.copy(source / f, tmp_path)
+
+            subprocess.run([sys.executable, self.root / "athletes.py"], cwd=tmp_path)
+            comp = filecmp.cmpfiles(source, tmp_path, ["athletes-sorted.yaml"], shallow=False)
+            assert not comp[1] and not comp[2]
