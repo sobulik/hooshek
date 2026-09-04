cf477635693e7ff9685cfff75140c46314c5f77f
sobulik <69842563+sobulik@users.noreply.github.com>
2026-09-04 19:03:57 +0200

    CLI with typer

--- tests/test_athletes.py
+++ tests/test_athletes.py
@@ -1,18 +1,22 @@
-import subcommand
+import hooshek.cli
 
 import filecmp
 import pytest
 import shutil
+import typer.testing
 
 
-def test_sort(project_path, fixture_path, tmp_path):
+def test_sort(fixture_path, tmp_path, monkeypatch):
     if not (fixture_path / "athletes-sorted.yaml").is_file():
         pytest.skip("No athletes-sorted.yaml for this fixture")
     for f in ("event.yaml", "clubs.yaml", "athletes.yaml"):
         shutil.copy(fixture_path / f, tmp_path)
+    monkeypatch.chdir(tmp_path)
 
-    subcommand.run(["athletes.py"], project_path, tmp_path)
+    runner = typer.testing.CliRunner()
+    result = runner.invoke(hooshek.cli.app, ["athletes"])
 
+    assert result.exit_code == 0
     assert filecmp.cmp(
         fixture_path / "athletes-sorted.yaml",
         tmp_path / "athletes-sorted.yaml",
