777bc887c382cab6e76c654e1d355df9876cad57
sobulik <69842563+sobulik@users.noreply.github.com>
2026-09-22 20:39:11 +0200

    assign-bibs as a typer command

--- src/hooshek/cli.py
+++ src/hooshek/cli.py
@@ -4,8 +4,10 @@ import hooshek.clubs.repo
 import hooshek.event.io
 import hooshek.startlist.io
 
+import collections
 import csv
 import datetime
+import hashlib
 import random
 import string
 import typer
@@ -200,9 +202,74 @@ def import_athletes(
     hooshek.athletes.io.dump(aths, "athletes-with-imported.yaml")
 
 
-@app.command("assing-bibs")
+@app.command("assign-bibs")
 def assign_bibs():
-    pass
+    event = hooshek.event.io.load()
+    clubs = hooshek.clubs.repo.load()
+
+    aths = hooshek.athletes.io.build(clubs, False)
+    athsWithId = tuple(filter(lambda x: hasattr(x, "id"), aths))
+
+    # expect no id or id == "0"
+    idCounter = collections.Counter(map(lambda x: x.id, athsWithId))
+    for i in idCounter:
+        if i != "0" and idCounter[i] > 0:
+            raise Exception(
+                "Athletes file athlete id "
+                + i
+                + " defined "
+                + str(idCounter[i])
+                + " times. Only zeros are expected!"
+            )
+
+    bib_red = 0
+    bib_black = 100
+    for race in event.races:
+        raceAths = list()
+        for athlete in athsWithId:
+            if (
+                athlete.sex == race.sex
+                and event.eff_year - athlete.born >= race.age_min
+                and event.eff_year - athlete.born <= race.age_max
+            ):
+                raceAths.append(
+                    (
+                        athlete,
+                        hashlib.md5(
+                            "{0}{1}{2}".format(
+                                athlete.name, athlete.surname, event.date
+                            ).encode()
+                        ).hexdigest(),
+                    )
+                )
+        raceAths.sort(key=lambda t: t[1])
+        for t in raceAths:
+            if t[0].born > event.eff_year - 10:
+                bib_red += 1
+                t[0].id = str(bib_red)
+            else:
+                bib_black += 1
+                while bib_black in (25, 235):
+                    bib_black += 1
+                t[0].id = str(bib_black)
+
+    # assert
+    idCounter = collections.Counter(map(lambda x: x.id, athsWithId))
+    for i in idCounter:
+        if i == "0":
+            raise Exception(
+                "Athletes file athlete id " + i + " not numerated correctly"
+            )
+        if idCounter[i] > 1:
+            raise Exception(
+                "Athletes file athlete id "
+                + i
+                + " defined "
+                + str(idCounter[i])
+                + " times"
+            )
+
+    hooshek.athletes.io.dump(aths, "athletes-with-bibs.yaml")
 
 
 if __name__ == "__main__":
--- tests/test_assign-bibs.py
+++ tests/test_assign-bibs.py
@@ -1,19 +1,23 @@
-import subcommand
+import hooshek.cli
 
 import filecmp
 import pytest
 import shutil
+import typer.testing
 
 
-def test_assign_bibs(project_path, fixture_path, tmp_path):
+def test_assign_bibs(fixture_path, tmp_path, monkeypatch):
     if not (fixture_path / "athletes-without-bibs.yaml").is_file():
         pytest.skip("No athletes-without-bibs.yaml for this fixture")
     for f in ("event.yaml", "clubs.yaml"):
         shutil.copy(fixture_path / f, tmp_path)
     shutil.copy(fixture_path / "athletes-without-bibs.yaml", tmp_path / "athletes.yaml")
+    monkeypatch.chdir(tmp_path)
 
-    subcommand.run(["assign-bibs.py"], project_path, tmp_path)
+    runner = typer.testing.CliRunner()
+    result = runner.invoke(hooshek.cli.app, ["assign-bibs"])
 
+    assert result.exit_code == 0
     assert filecmp.cmp(
         fixture_path / "athletes-with-bibs.yaml",
         tmp_path / "athletes-with-bibs.yaml",
